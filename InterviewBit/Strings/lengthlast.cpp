5c8f775389a2969e0bddb89d2c937f898a05d2d6
Abhijeet Nayak <abnayak@outlook.com>
2015-08-02 14:23:19 -0700

    Added interview bit string lengthlast problem

---
+++ InterviewBit/Strings/lengthlast.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int lengthOfLastWord(const string &A) {
+    string lastWord = "";
+
+    for (int i=0;i < A.size();) {
+      if (A[i] == ' ') {
+        i++;
+      }
+      if (isalpha(A[i])) {
+        int begin = i;
+        while (isalpha(A[++i]) && i <= A.size());
+        lastWord = A.substr(begin, i-begin);
+      }
+    }
+    return lastWord.size();
+}
+
+
+
+int main() {
+  cout << lengthOfLastWord("") << endl;
+  return 0;
+}
