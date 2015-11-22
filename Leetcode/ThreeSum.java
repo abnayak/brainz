c7dc55836e11f3a4d1d353fe6a8e5f9377d0465e
abnayak <abnayak@outlook.com>
2015-11-22 10:54:22 -0800

    Renamed file

--- Leetcode/ThreeSum.java
+++ Leetcode/ThreeSum.java
@@ -1,16 +1,13 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
- * Created by abhijeet on 11/8/2015.
+ * Created by abhijeet on 11/10/2015.
  */
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] list = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
+        int[] list = {1,-1,-1,0}; // -1 -1 0 1
         Solution solution = new Solution();
         List<List<Integer>> result = solution.threeSum(list);
         for(List<Integer> i : result) {
@@ -20,60 +17,41 @@ public class ThreeSum {
             System.out.println("\n");
         }
     }
-
     public static class Solution {
-
         public List<List<Integer>> threeSum(int[] nums) {
             List<List<Integer>> solution = new ArrayList<>();
             Arrays.sort(nums);
-            int begin = 0 ;
-            int end = nums.length-1;
-            Set set = new HashSet();
+            int len = nums.length-3;
+            for(int i = 0; i <= len ;) {
 
-            while (begin < end) {
-                int remains = 0 - (nums[begin]+nums[end]);
-                int found = binarySearch(nums, begin+1, end-1, remains);
+                int low = i+1;
+                int high = nums.length-1;
 
-                if (found != -1) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[begin]);
-                    list.add(nums[found]);
-                    list.add(nums[end]);
-
-                    String key = Arrays.toString(list.toArray());
-                    if (!set.contains(key)) {
-                        set.add(key);
-                        solution.add(list);
+                while(low < high) {
+                    if (nums[i] + nums[low] + nums[high] > 0) {
+                        high--;
+                    } else if (nums[i] + nums[low] + nums[high] < 0) {
+                        low++;
+                    } else if(nums[i] + nums[low] + nums[high] == 0) {
+                        List<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[low]);
+                        temp.add(nums[high]);
+                        solution.add(temp);
+                        low++;
+                        high--;
+                        while(low < high && nums[low] == nums[low-1] && nums[high] == nums[high+1]) {
+                            low++;
+                            high--;
+                        }
                     }
                 }
-
-                if (remains > 0){
-                    begin++;
-                } else if (remains == 0) {
-                    begin++;
-                    end++;
+                i++;
+                while (i <= len && nums[i] == nums[i-1]) {
+                    i++;
                 }
-                else {
-                    end--;
-                }
-
             }
             return solution;
-        }
-
-        public int binarySearch(int[] nums, int begin, int end, int searchMe) {
-            while (begin <= end) {
-                int mid = (begin+end)/2;
-                 if(nums[mid] == searchMe) {
-                     return mid;
-                 } else if (searchMe < nums[mid]) {
-                    end = mid-1;
-                } else {
-                     begin = mid+1;
-                 }
-
-            }
-            return -1;
         }
     }
 }
