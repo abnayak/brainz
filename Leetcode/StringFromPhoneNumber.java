80f4c639a37684e3c1f390f2e3b54de11a60043d
abnayak <abnayak@outlook.com>
2016-01-31 07:30:34 -0800

    Adding java problems

--- Leetcode/StringFromPhoneNumber.java
+++ Leetcode/StringFromPhoneNumber.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by abhijeet on 11/22/2015.
+ *
+ * Given a digit string, return all possible letter combinations that the number could represent.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.reated by abhijeet on 11/22/2015.
+ * Input:Digit string "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  */
 public class StringFromPhoneNumber {
+    public class Solution {
+        private String[] numbers =  {};
+        private List<String> solution = new ArrayList<>();
+        public List<String> letterCombinations(String digits) {
+
+        }
+    }
 }
