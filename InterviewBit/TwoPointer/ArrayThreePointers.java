e8f310e797d784b6ce6f4b23e56b205f7a199aca
abnayak <abnayak@outlook.com>
2016-10-04 22:36:47 -0700

    new problem

--- InterviewBit/TwoPointer/ArrayThreePointers.java
+++ InterviewBit/TwoPointer/ArrayThreePointers.java
@@ -16,10 +16,30 @@ public class ArrayThreePointers {
             int i = 0;
             int j = 0;
             int k = 0;
-            long min = Long.MAX_VALUE;
+            int min = Integer.MAX_VALUE;
 
-            while(i )
+            int ai;
+            int bj;
+            int ck;
+            int MINIMUM;
+            int MAXIMUM;
+            while (i < a.size() && j < b.size() && k < c.size()) {
+                ai = a.get(i);
+                bj = b.get(j);
+                ck = c.get(k);
 
+                MAXIMUM = Math.max(ai, Math.max(bj, ck));
+                MINIMUM = Math.min(ai, Math.min(bj, ck));
+
+                // Assign the new min
+                min = MAXIMUM - MINIMUM < min ? MAXIMUM - MINIMUM : min;
+
+                // If a[i] is less than other two
+                if (ai == MINIMUM) i++;
+                if (bj == MINIMUM) j++;
+                if (ck == MINIMUM) k++;
+            }
+            return min;
         }
     }
 
