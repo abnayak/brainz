ad51c1d4aba4717b4329ca8e1e133bfe38e088ff
abnayak <abnayak@outlook.com>
2016-12-04 21:20:43 -0800

    more problems

---
+++ InterviewBit/Hashing/Anagrams.java
@@ -0,0 +1,47 @@
+package Hashing;
+
+import sun.util.resources.cldr.zh.CalendarData_zh_Hans_HK;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by abhijeet on 12/4/16.
+ */
+public class Anagrams {
+    public static void main(String[] args) {
+
+    }
+
+    public class Solution {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        public ArrayList<ArrayList<Integer>> anagrams(final List<String> a) {
+            if (a == null || a.size() == 0) return list;
+            HashMap<String, ArrayList<Integer>> hashMap = new HashMap<>();
+            for (int i = 0; i < a.size(); i++) {
+                String s = a.get(i);
+                char[] array = s.toCharArray();
+                Arrays.sort(array);
+                String str = new String(array);
+                if (hashMap.containsKey(str)) {
+                    ArrayList<Integer> list = hashMap.get(str);
+                    list.add(i+1);
+                    hashMap.put(str, list);
+                } else {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(i+1);
+                    hashMap.put(str, list);
+                }
+            }
+
+            for(String s : hashMap.keySet()) {
+                list.add(hashMap.get(s));
+            }
+            return list;
+        }
+    }
+}
